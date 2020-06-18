4120a360eacd1ff11e02e4dfc8c85f0a6a6ad653
Ruben <rubentrevino95@gmail.com>
2020-06-17 19:42:49 -0500

    learning py

---
+++ playground.py
@@ -0,0 +1,40 @@
+name = "Ruben"
+print("Hello " + name)
+
+
+# slicing strings
+word = "Hello world."
+print(word[2:5])
+
+def sayHello(name):
+    print("Hello " + name + "!")
+
+
+sayHello("Ruben")
+
+
+# Function
+def sum(a, b):
+    print(a+b)
+
+
+sum(3, 5)
+
+
+def length(word):
+    print(len(word))
+
+
+length("Ruben")
+
+# Looping array
+cars = ["ford", "toyota", "volvo"]
+def firstCar(cars):
+    for x in cars:
+        print(x.upper())
+
+
+firstCar(cars)
+
+
+
