4a48b109da5daa904ba87560e5f137d04581815b
Ruben <rubentrevino95@gmail.com>
2020-06-18 23:40:36 -0500

    learning data types

---
+++ data_types.py
@@ -0,0 +1,38 @@
+a = 10
+b = 3.14
+c = "Hello World"
+d = 'tinker'
+
+
+print(a + a)
+
+print(d[1:4])
+
+# Data Types
+print(type(a))
+print(type(b))
+print(type(c))
+
+# length of string
+print(len(c))
+
+# index of string
+print (c[1])
+
+# slicing
+print (c[:3])
+
+# sub section slicing
+print (c[3:6])
+
+# beginning to end rev
+print (c[::-1])
+
+# beginning to end step size in 2s
+print (c[::2])
+
+# index to index step size in 2s
+print (c[0:6:2])
+
+
+
