addb8c63d288c75ddaed6d446fe41fd4d7b7765a
Ruben <rubentrevino95@gmail.com>
2020-06-27 12:58:14 -0500

    leanring flat loops

--- exercises/lists.py
+++ exercises/lists.py
@@ -40,3 +40,37 @@ letter_list.sort()
 num_list.sort()
 print (letter_list)
 print (num_list)
+
+# FLATTENED LOOPS
+
+# append to list
+my_string = 'hello'
+# element for element
+my_append = [x for x in my_string]
+
+print (my_append)
+
+# square every even number
+lisst = [x**2 for x in range(0,11) if x % 2 == 0]
+
+print (lisst)
+
+# FIZZBUZZ
+# fizz_list = []
+# fizzbuzz = [x for x in range(0,101)]
+#
+# print (fizzbuzz)
+
+
+# nested loops
+nested = []
+for x in [2,4,6]:
+    for y in [100, 200, 300]:
+        nested.append(x*y)
+
+        print (nested)
+
+# in one line nested lists
+nested_flat = [x*y for x in [2,4,6] for y in [40,60,80]]
+
+print (nested_flat)
