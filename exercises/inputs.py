bedfb81d3be94e03db993b0ab1caf973cdf3b053
Ruben <rubentrevino95@gmail.com>
2020-06-26 22:23:40 -0500

    playing with user input

---
+++ exercises/inputs.py
@@ -0,0 +1,17 @@
+result = input('enter a response here: ')
+
+# print (type(result))
+
+# type cast an input result
+# int(result)
+# float(result)
+
+
+def talk(result):
+    if result == 'hello':
+        print ('whats up!')
+    else:
+        print ('You can start off with saying hi!')
+
+
+talk(result)
