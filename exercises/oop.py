d6ad35300bd08264434bb91a69a976c2344dc95e
Ruben <rubentrevino95@gmail.com>
2020-07-16 13:57:20 -0500

    practicing web automation

--- exercises/oop.py
+++ exercises/oop.py
@@ -1,11 +1,27 @@
-
-
+# class name
 class Person():
+    # class object attribute
+    # same for any instance of a class
+    species = 'human'
 
-    def __init__(self, name):
+    # constructor
+    def __init__(self, name, gender, age):
         self.name = name
+        self.gender = gender
+        self.age = age
+
+    # method
+    def speak(self):
+        print ("Hello! My name is {}".format(self.name))
 
 
-boy = Person()
+# creating instance of object
+ruben = Person(name='Ruben', gender='male', age=24)
 
-print(type(boy))
+# print(type(ruben))
+
+print (ruben.name, ruben.gender, ruben.age, ruben.species)
+
+print (ruben.speak())
+
+# methods are function in classes
