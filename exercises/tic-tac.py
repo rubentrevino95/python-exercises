7dc2388a0706ef382822469fb6836360ee25fac6
Ruben <rubentrevino95@gmail.com>
2020-12-31 03:28:46 -0600

    prepping inputs

---
+++ exercises/tic-tac.py
@@ -0,0 +1,20 @@
+def display(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+row1 = [' ',' ',' ']
+row2 = [' ',' ',' ']
+row3 = [' ',' ',' ']
+
+print(row1,row2,row3)
+
+position_index = int(input('Please enter a value: '))
+
+def user_choice():
+
+    choice = input('Please enter a number (0-10): ')
+
+    return int(choice)
+
+print(user_choice())
