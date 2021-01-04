b0ed84646c27b7d5931a56fd4c46fd7a8aaeb355
Ruben <rubentrevino95@gmail.com>
2021-01-03 20:36:35 -0600

    adding work

--- exercises/tic-tac.py
+++ exercises/tic-tac.py
@@ -13,7 +13,14 @@ position_index = int(input('Please enter a value: '))
 
 def user_choice():
 
-    choice = input('Please enter a number (0-10): ')
+    choice = 'WRONG'
+
+    while choice.isdigit() == False:
+
+        choice = input('Please enter a number (0-10): ')
+
+        if choice.isdigit() == False:
+            print('Sorry that is not a number.')
 
     return int(choice)
 
